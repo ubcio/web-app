32e5bff7b9b59ec892407b421cc8d5777eb428be
Arie Milner <amilner42@gmail.com>
2015-10-27 00:22:45 -0700

    Prototype working for degree requirements page

--- client/src/app/degree-requirements/degree-requirements.js
+++ client/src/app/degree-requirements/degree-requirements.js
@@ -25,25 +25,34 @@
   function DegreeRequirementsCtrl() {
     var degreeRequirements = this;
     degreeRequirements.degrees = [
-      {name: "Computer Science Major", scroll: 100},
-      {name: "Computer Science Honours", scroll: 100},
-      {name: "Computer Science and Another Science Subject Combined Major", scroll: 100},
-      {name: "Computer Science and Biology Combined Major" , scroll: 100},
-      {name: "Computer Science and Mathematics Combined Major", scroll: 100},
-      {name: "Computer Science and Microbiology and Immunology Combined Major", scroll: 100},
-      {name: "Computer Science and Physics Combined Major" , scroll: 100},
-      {name: "Computer Science and Statistics Combined Major" , scroll: 100},
-      {name: "Atmospheric Science and Computer Science Combined Honours" , scroll: 100},
-      {name: "Computer Science and Biology Combined Honours" , scroll: 100},
-      {name: "Computer Science and Mathematics Combined Honours", scroll: 100},
-      {name: "Computer Science and Microbiology and Immunology Combined Honours", scroll: 100},
-      {name: "Computer Science and Physics Combined Honours" , scroll: 100},
-      {name: "Computer Science and Statistics Combined Honours" , scroll: 100},
-      {name: "Computer Science and Software Engineering Major" , scroll: 100},
-      {name: "Computer Science and Software Engineering Honours" , scroll: 100},
-      {name: "Cognitive Systems Major: Computational Intelligence and Design", scroll: 100}
+      {name: "Computer Science Major", scroll: 2275},
+      {name: "Computer Science Honours", scroll: 2650},
+      {name: "Computer Science and Another Science Subject Combined Major", scroll: 3200},
+      {name: "Computer Science and Biology Combined Major" , scroll: 3920},
+      {name: "Computer Science and Mathematics Combined Major", scroll: 3970},
+      {name: "Computer Science and Microbiology and Immunology Combined Major", scroll: 4020},
+      {name: "Computer Science and Physics Combined Major" , scroll: 4070},
+      {name: "Computer Science and Statistics Combined Major" , scroll: 5090},
+      {name: "Atmospheric Science and Computer Science Combined Honours" , scroll: 5965},
+      {name: "Computer Science and Biology Combined Honours" , scroll: 6020},
+      {name: "Computer Science and Mathematics Combined Honours", scroll: 6075},
+      {name: "Computer Science and Microbiology and Immunology Combined Honours", scroll: 6990},
+      {name: "Computer Science and Physics Combined Honours" , scroll: 7040},
+      {name: "Computer Science and Statistics Combined Honours" , scroll: 8100},
+      {name: "Computer Science and Software Engineering Major" , scroll: 9300},
+      {name: "Computer Science and Software Engineering Honours" , scroll: 9760},
+      {name: "Cognitive Systems Major: Computational Intelligence and Design", scroll: 10300}
     ];
     degreeRequirements.selected = undefined;
+
+    degreeRequirements.scrollFrame = function(scrollToX) {
+      if(scrollToX != null) {
+         document.getElementById('frameWrapper').scrollTop = scrollToX;
+      } else {
+        // TODO
+        // error message - "please select a degree from the dropdown"
+      }
+    }
 
   }
 
