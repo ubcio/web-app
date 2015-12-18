cffb008be42ec17c8814993d8eb09e9c604b6644
Arie Milner <amilner42@gmail.com>
2015-12-18 13:19:28 -0800

    Made team members shuffled

--- client/src/app/the-team/the-team.js
+++ client/src/app/the-team/the-team.js
@@ -25,8 +25,26 @@
   function TheTeamCtrl(TheTeamService , $scope) {
     var theTeam = this;
 
+    // Fisher–Yates shuffle algorithm
+    var shuffleArray = function(array) {
+      var m = array.length, t, i;
+
+      // While there remain elements to shuffle
+      while (m) {
+        // Pick a remaining element…
+        i = Math.floor(Math.random() * m--);
+
+        // And swap it with the current element.
+        t = array[m];
+        array[m] = array[i];
+        array[i] = t;
+      }
+
+      return array;
+    };
+
     var postInit = function(teamData) {
-      theTeam.teamMembers = teamData.teamMembers;
+      theTeam.teamMembers = shuffleArray(teamData.teamMembers);
       theTeam.loaded = true;
       $scope.$applyAsync();
     };
